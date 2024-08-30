a238ae1e1637bb1c7230896060a115cda81f4357
luamondstock <134516705+luamondstock@users.noreply.github.com>
2024-08-30 12:02:29 -0300

    Create c0ont.c

---
+++ c0ont.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    for (int i = 0; i <= 20; i ++){
+        {if (i%2 == 1)
+            printf("%d,", i);
+        }
+    }
+    
+    return 0;
+}
